98967eee46bcf69215c5308946e74417f657c290
lsj1124 <44527365+lsj1124@users.noreply.github.com>
2022-11-10 20:32:18 -0600

    add search restaurant function

--- website/urls.py
+++ website/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('restaurant/', views.restaurant, name="restaurant"),
 ]
